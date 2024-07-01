f6385c9318e83508e2de62c466245fc0256ca946
Isha Ingle <ingle.isha@gmail.com>
2024-07-01 12:00:26 +0530

    Initial commit 1

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,26 +2,43 @@ class BookingsController < ApplicationController
 	before_action :authenticate_user!
 
 	def create
-	    @showtime = Showtime.find(params[:showtime_id])
-	    if @showtime.available_seats > 0
-		    seat_number = (@showtime.bookings.maximum(:seat_number) || 0) + 1
-		    @booking = @showtime.bookings.new(seat_number: seat_number, user: current_user)
-		    if @booking.save
-		        @showtime.update(available_seats: @showtime.available_seats - 1)
-		        redirect_to movie_showtime_path(@showtime.movie, @showtime), notice: "Booking confirmed. Seat number: #{seat_number}"
-		    else
-		        redirect_to movie_showtime_path(@showtime.movie, @showtime), alert: "Failed to book seat."
-		    end
+	  @showtime = Showtime.find(params[:showtime_id])
+		if @showtime.available_seats > 0
+	    canceled_booking = @showtime.bookings.find_by(status: 'canceled')
+
+	    if canceled_booking.present?
+      	seat_number = canceled_booking.seat_number
+      	canceled_booking.update(status: 'booked', user: current_user)
+      	@showtime.update(available_seats: @showtime.available_seats - 1)
 	    else
-	      	redirect_to movie_showtime_path(@showtime.movie, @showtime), alert: "No seats available."
+      	seat_number = (@showtime.bookings.maximum(:seat_number) || 0) + 1
+      	@booking = @showtime.bookings.new(seat_number: seat_number, user: current_user, status: 'booked')
+
+      	if @booking.save
+        	@showtime.update(available_seats: @showtime.available_seats - 1)
+        	redirect_to movie_showtime_path(@showtime.movie, @showtime), notice: "Booking confirmed. Seat number: #{seat_number}"
+        	return
+      	else
+        	redirect_to movie_showtime_path(@showtime.movie, @showtime), alert: "Failed to book seat."
+        	return
+      	end
 	    end
+
+	    redirect_to movie_showtime_path(@showtime.movie, @showtime), notice: "Booking confirmed. Seat number: #{seat_number}"
+		else
+		  redirect_to movie_showtime_path(@showtime.movie, @showtime), alert: "No seats available."
+		end
 	end
 
-  	def destroy
-	    @booking = Booking.find(params[:id])
-	    @showtime = @booking.showtime
-	    @booking.destroy
-	    @showtime.update(available_seats: @showtime.available_seats + 1)
-	    redirect_to movie_showtime_path(@showtime.movie, @showtime), notice: "Booking cancelled."
+	def destroy
+  	@booking = Booking.find(params[:id])
+  	@showtime = @booking.showtime
+  
+  	if @booking.update(status: 'canceled')
+    	@showtime.update(available_seats: @showtime.available_seats + 1)
+    	redirect_to movie_showtime_path(@showtime.movie, @showtime), notice: "Booking canceled."
+  	else
+    	redirect_to movie_showtime_path(@showtime.movie, @showtime), alert: "Failed to cancel booking."
   	end
+	end
 end
